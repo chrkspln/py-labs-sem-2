720d3cc643e7591a5c98d9d61f9181625a334699
Polina <polina_cherkasova@outlook.com>
2024-02-27 13:03:45 +0200

    +1 lab

--- array-zigzag-traversal-lab-and-tests.py
+++ array-zigzag-traversal-lab-and-tests.py
@@ -17,6 +17,7 @@
 import unittest
 
 
+# діагональ вправо-вгору, межі - до верху масиву (і = 0)
 def diag_up(i, j, n, m, arr, result_arr):
     while 0 < i <= n - 1 and 0 <= j <= m - 2:
         i -= 1
@@ -26,6 +27,7 @@ def diag_up(i, j, n, m, arr, result_arr):
     return i, j
 
 
+# діагональ вліво-вниз, межі - до лівого краю масиву (j = 0)
 def diag_down(i, j, n, m, arr, result_arr):
     while 0 <= i <= n - 2 and 0 < j <= m - 1:
         i += 1
@@ -35,17 +37,17 @@ def diag_down(i, j, n, m, arr, result_arr):
     return i, j
 
 
-def go_right(i, j, n, m, arr, result_arr):
-    if j != m - 1:
-        j += 1
+def go_left(i, j, n, m, arr, result_arr):
+    if 0 < j <= m - 1:
+        j -= 1
         curr_val = arr[i][j]
         result_arr.append(curr_val)
     return i, j
 
 
-def go_down(i, j, n, m, arr, result_arr):
-    if i != n - 1:
-        i += 1
+def go_up(i, j, n, m, arr, result_arr):
+    if i != 0:
+        i -= 1
         curr_val = arr[i][j]
         result_arr.append(curr_val)
     return i, j
@@ -55,41 +57,39 @@ def arr_zigzag_traverse(arr, n, m):
     result_arr = []
     # ЛОКАЛЬНІ і та j це значення індексів в масиві, де і відповідає за ряки (n)
     # а j - за стовпці (m)
-    i = 0
-    j = 0
+    i = n - 1
+    j = m - 1
     curr_val = arr[i][j]    # поточне значення (1), записується в одновимірний масив
     result_arr.append(curr_val)
 
-    # якшо масив де-факто одновимірний виводим його
+    # якшо масив де-факто одновимірний то виводим його
     if m == 1:
-        return arr
+        for i in range(1, len(arr)):
+            result_arr.append(arr[i])
+        return result_arr
 
     # цикл дійсний поки і в межах рядків, а j - в межах стовпців
     while 0 <= i <= n - 1 and 0 <= j <= m - 1:
         # якшо стоїмо на передостанньому елементі,
-        # просто рухаємся вправо і виходим
-        if i == n - 1 and j == m - 2:
-            go_right(i, j, n, m, arr, result_arr)
+        # просто рухаємся вліво і виходим
+        if i == 0 and j == 1:
+            go_left(i, j, n, m, arr, result_arr)
             return result_arr
-        # якшо стоїмо на першому рядку, але не останньому стовпчику ->
-        # -> рухаємся вправо а потім по діагоналі вниз
-        elif i == 0 and j >= 0 and j != m - 1:
-            i, j = go_right(i, j, n, m, arr, result_arr)
+
+        elif i == n - 1 and 0 < j <= m - 1:
+            i, j = go_left(i, j, n, m, arr, result_arr)
+            i, j = diag_up(i, j, n, m, arr, result_arr)
+
+        elif 0 < i <= n - 1 and j == m - 1:
+            i, j = go_up(i, j, n, m, arr, result_arr)
             i, j = diag_down(i, j, n, m, arr, result_arr)
-        # якшо стоїмо на останньому рядку, але не на останньому стовпчику ->
-        # -> рухаємся вправо і по діагоналі вгору
-        elif i == n - 1 and j >= 0 and j != m - 1:
-            i, j = go_right(i, j, n, m, arr, result_arr)
+
+        elif 0 < i <= n - 1 and j == 0:
+            i, j = go_up(i, j, n, m, arr, result_arr)
             i, j = diag_up(i, j, n, m, arr, result_arr)
-        # якшо стоїмо на крайньому лівому стовпчику ->
-        # -> рухаємся вниз на рядок а потім по діагоналі вгору
-        elif i != n - 1 and j == 0:
-            i, j = go_down(i, j, n, m, arr, result_arr)
-            i, j = diag_up(i, j, n, m, arr, result_arr)
-        # якшо стоїмо на останньому стовпчику ->
-        # -> рухаємся вниз на рядок і по діагоналі вниз
-        elif j == m - 1:
-            i, j = go_down(i, j, n, m, arr, result_arr)
+
+        elif i == 0:
+            i, j = go_left(i, j, n, m, arr, result_arr)
             i, j = diag_down(i, j, n, m, arr, result_arr)
 
 
@@ -100,9 +100,6 @@ def arr_zigzag_traverse(arr, n, m):
 
 class Lab1Test(unittest.TestCase):
     def test_arr_zigzag_traverse_5x5(self):
-        # тут воно матюкається на out_of_range хоча якшо запустити
-        # цей же масив не в форматі тесту, а просто -
-        # все правильно працює
         test_arr = [
             [1, 2, 3, 4, 5],
             [6, 7, 8, 9, 10],
@@ -112,13 +109,12 @@ class Lab1Test(unittest.TestCase):
         ]
         test_n = 5
         test_m = 5
-        needed_result = [1, 2, 6, 11, 7, 3, 4, 8, 12, 16, 21, 17, 13,
-                         9, 5, 10, 14, 18, 22, 23, 19, 15, 20, 24, 25]
+        needed_result = [25, 24, 20, 15, 19, 23, 22, 18, 14, 10, 5,
+                         ]
         self.assertEqual(arr_zigzag_traverse(test_arr, test_n, test_m),
                          needed_result, "error!")
 
     def test_arr_zigzag_traverse_4x2(self):
-        # тут воно по трохи дивній траєкторії йде але хай буде
         test_arr = [
             [1, 2],
             [3, 4],
@@ -148,7 +144,6 @@ class Lab1Test(unittest.TestCase):
                          needed_result, "error!")
 
     def test_arr_zigzag_traverse_1x1(self):
-        # тут те саме, по факту все норм але тесту не подобається
         test_arr = [
             [1]
         ]
@@ -161,15 +156,3 @@ class Lab1Test(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    #   arr = [
-    #       [1, 2, 3, 4, 5],
-    #       [6, 7, 8, 9, 10],
-    #       [11, 12, 13, 14, 15],
-    #       [16, 17, 18, 19, 20],
-    #       [21, 22, 23, 24, 25]
-    #   ]
-    #   n = 5
-    #   m = 5
-    #   result = arr_zigzag_traverse(arr, n, m)
-    #   for i in result:
-    #       print(i)
