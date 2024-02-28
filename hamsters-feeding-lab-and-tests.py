238bd7008d9d66478c2d7ae277c96cc0525bff78
Polina <polina_cherkasova@outlook.com>
2024-02-28 15:26:02 +0200

    fixed the algorithm and added comments to the code

--- hamsters-feeding-lab-and-tests.py
+++ hamsters-feeding-lab-and-tests.py
@@ -41,15 +41,51 @@ import unittest
 
 
 def how_many_hamsters(hamsters_list, hamsters_quantity, daily_food):
+
+    # якщо список пустий - повертаємо 0
+    if not hamsters_list:
+        return 0
+
+    # сортуємо список хом'ячків за жадібністю, від меншої до більшої
     hamsters_list = sorted(hamsters_list, key=lambda arr: arr[1])
-    food_needed = 0
-    hamsters_counter = 0
+
+    adopted_hamsters_list = []
+    food_already_needed = 0
+
+    # змінна для проходження по кожному рядку (кожному хом'ячку)
     _ = 0
-    while food_needed < daily_food and _ < hamsters_quantity:
-        food_needed += hamsters_list[_][0] + hamsters_list[_][1] * hamsters_quantity
-        hamsters_counter += 1
+    while food_already_needed < daily_food and _ < hamsters_quantity:
+
+        # якщо ще ні одного хом'ячка не було додано до списку - додаєм найпершого,
+        # бо він має найменшу жадібність з усіх наявних
+        if _ == 0:
+            adopted_hamsters_list.append([hamsters_list[_][0], hamsters_list[_][1]])
+        else:
+
+            # для уже наявних хом'ячків в списку на одомашнення
+            # вираховуємо сумарну кількість їжі:
+            # сумарна к-ть += їжа для одного + (жадібність * кількість всіх хом'ячків)
+            # чому ВСІХ у списку, хоча хом'ячок їсть лише за своїх сусідів,
+            # а за себе жадібність не рахується?
+            # бо по факту формула іде (...) * len(adopted - 1 + 1):
+            # мінус хом'ячок для якого рахуєм,
+            # плюс той, кого хочемо додати. виходить 0
+            for food_for_one, greedy_food in adopted_hamsters_list:
+                food_already_needed += food_for_one + greedy_food * len(adopted_hamsters_list)
+
+            # додаємо до сумарної кількості норму в день на того хом'ячка,
+            # якого хочемо додати. логіка формули аналогічна
+            food_already_needed += hamsters_list[_][0] + hamsters_list[_][1] * len(adopted_hamsters_list)
+            if food_already_needed < daily_food:
+                adopted_hamsters_list.append([hamsters_list[_][0], hamsters_list[_][1]])
+                food_already_needed = 0
+
+        # незалежно від операцій змінна йде += 1
+        # аби перейти до наступного хом'ячка
         _ += 1
-    return hamsters_counter
+
+    # ф-ія повертає кількість хом'ячків на одомашнення
+    return len(adopted_hamsters_list)
 
 
 class Lab2Test(unittest.TestCase):
@@ -61,8 +97,7 @@ class Lab2Test(unittest.TestCase):
             [2, 2],
             [3, 1]
         ]
-        expected_result = 2
-        self.assertEqual(how_many_hamsters(hamsters, C, S), expected_result, "error!")
+        self.assertEqual(how_many_hamsters(hamsters, C, S), 2, "error!")
 
     def test_example2(self):
         S = 19
@@ -73,8 +108,7 @@ class Lab2Test(unittest.TestCase):
             [1, 4],
             [5, 1]
         ]
-        expected_result = 3
-        self.assertEqual(how_many_hamsters(hamsters, C, S), expected_result, "error!")
+        self.assertEqual(how_many_hamsters(hamsters, C, S), 3, "error!")
 
     def test_example3(self):
         S = 2
@@ -85,6 +119,35 @@ class Lab2Test(unittest.TestCase):
         ]
         expected_result = 1
         self.assertEqual(how_many_hamsters(hamsters, C, S), expected_result, "error!")
+
+# gpt tests
+    def test_basic_case(self):
+        hamsters_list = [(10, 5), (8, 4), (7, 3)]
+        hamsters_quantity = 3
+        daily_food = 30
+        expected_result = 2
+        self.assertEqual(how_many_hamsters(hamsters_list, hamsters_quantity, daily_food), expected_result)
+
+    def test_no_hamsters_needed(self):
+        hamsters_list = [(10, 5), (8, 4), (7, 3)]
+        hamsters_quantity = 3
+        daily_food = 5
+        expected_result = 1
+        self.assertEqual(how_many_hamsters(hamsters_list, hamsters_quantity, daily_food), expected_result)
+
+    def test_empty_hamsters_list(self):
+        hamsters_list = []
+        hamsters_quantity = 5
+        daily_food = 30
+        expected_result = 0
+        self.assertEqual(how_many_hamsters(hamsters_list, hamsters_quantity, daily_food), expected_result)
+
+    def test_zero_daily_food(self):
+        hamsters_list = [(10, 5), (8, 4), (7, 3)]
+        hamsters_quantity = 3
+        daily_food = 0
+        expected_result = 0
+        self.assertEqual(how_many_hamsters(hamsters_list, hamsters_quantity, daily_food), expected_result)
 
 
 if __name__ == '__main__':
